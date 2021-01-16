36296eecebe547c0d961378a34954e68b6b72c2a
18865387113 <1194665823@qq.com>
2021-01-16 09:03:14 +0800

    init

---
+++ src/views/cms/book/create.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app-container">
+    新增书籍
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
---
+++ src/views/cms/book/list.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app-container">
+    书籍列表
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
